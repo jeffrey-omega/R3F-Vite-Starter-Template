dd90db00428fd7b3a2b74360391e9151171d6554
jeffrey-mostroso <>
2024-12-24 21:29:27 +0100

    Add Atom component to represent atomic structure in the scene

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,21 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
-import { ReactLogo } from "./Example";
+import { Atom } from "./Atom";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls
-        enableZoom={true}
-        //minDistance={6}
-        //maxDistance={19}
-        autoRotate={false}
-        autoRotateSpeed={-0.45}
-        target={[0, 0.5, 0]}
-        //minPolarAngle={1.3}
-        //maxPolarAngle={Math.PI / 2}
-        enablePan={false}
-      />
-      <ReactLogo position={[0, 0.5, 0]} />
+      <OrbitControls enableZoom={true} enablePan={false} />
+      <Atom />
     </>
   );
 };
